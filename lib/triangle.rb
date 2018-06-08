305d3b5bec3d0f7c4cb2445bdd01bc3c8076e5d4
Taylor Williams <taylor@thewilliams.net>
2018-06-08 04:57:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,13 +4,9 @@ class Triangle
   def initialize(one, two, three)
     @angles = [one, two, three].sort
     if @angles[0] == 0 || @angles[1] == 0 || @angles[2] == 0
-      begin
-        raise TriangleError
-      end
+      raise TriangleError
     elsif @angles[0] >= @angles[1] + @angles[2]
-      begin
-        raise TriangleError
-      end
+      raise TriangleError
     elsif @angles[0] == @angles[1] && @angles[0] == @angles[2]
       @kind = :equilateral
     elsif @angles[0] == @angles [1] || @angles[1] == @angles[2] || @angles[2] == @angles[0]
