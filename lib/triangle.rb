918aef7f03d4a9392b513dd7c6d70a713dcc199e
Taylor Williams <taylor@thewilliams.net>
2018-06-08 04:36:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :angles, :kind
+  def initialize(one, two, three)
+    @angles = [one, two, three].sort
+    case
+    when @angles[0] == 0 || @angles[1] == 0 || @angles[2] == 0
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts TriangleError.message
+      when @angles[0] >= @angles[1] + @angles[2]
+        begin
+          raise TriangleError
+        rescue TriangleError => error
+          puts TriangleError.message
+      when @angles[0] == @angles[1] == @angles[2]
+        @kind = "equilateral"
+      when 
+        
+    end
+      
+    end
+  end
+end
+class TriangleError < StandardError
+  # triangle error code
+  def message
+    "must be a valid triangle"
+  end
 end
